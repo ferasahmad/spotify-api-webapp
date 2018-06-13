5df4ef324e246183e4f044ec7fdf5e51908ba96a
Javier Almansa <j.almansa@wata.es>
2018-06-13 14:59:05 +0200

    no message

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('/'));
+app.use(express.static('www'));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join('/src/index.html'));
-});
+// app.get('*', function(req, res) {
+//     res.sendFile('/src/index.html');
+// });
 
 app.listen(process.env.PORT || 8080);
