51400682d9f68f1e42ade49ef68c75cda752e36f
Javier Almansa <j.almansa@wata.es>
2018-06-13 14:48:10 +0200

    check

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const app = express();
 
-// app.use(express.static(__dirname + '/'));
+app.use(express.static('/dist'));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname));
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join('/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
